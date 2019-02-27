9a01b24739680b41b37e38f252174bb8fcc5bc5f
Msarbel <35122452+msarbel@users.noreply.github.com>
2019-02-27 18:30:52 -0500

    changed up a bit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express= require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({bye: 'dude'});
+    res.send({bye: 'oh yeah'});
 });
 
 const PORT = process.env.PORT || 5000;
