3ad7df700f4c2915220960d86315ed6e87a8d9aa
Msarbel <35122452+msarbel@users.noreply.github.com>
2019-03-19 18:02:48 -0400

    adding send email functionality

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -12,7 +14,11 @@ module.exports = app => {
             title, 
             subject, 
             body,
-            
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
         });
+        // send email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
